29224bb32f2ae0baf3f6d3d9f9e33a8bd43bef70
FdelMazo <fdelmazo@fi.uba.ar>
2021-11-16 22:27:04 -0300

    Initial doc generation

---
+++ docs/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"alglobo":{"doc":"","t":[5],"n":["main"],"q":["alglobo"],"d":[""],"i":[0],"f":[[[]]],"p":[]},\
+"informe":{"doc":"Informe","t":[5],"n":["main"],"q":["informe"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ docs/doc/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["alglobo"] = {"name":"","files":["alglobo.rs"]};
+sourcesIndex["informe"] = {"name":"","files":["informe.rs"]};
+createSourceSidebar();
