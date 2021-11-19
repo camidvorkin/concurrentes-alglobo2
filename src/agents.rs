753bea460092ce58ed2dfe5a12b36722d9e32733
FdelMazo <fdelmazo@fi.uba.ar>
2021-11-19 14:26:43 -0300

    Create one thread per agent, per connection

--- src/agents.rs
+++ src/agents.rs
@@ -1,12 +1,83 @@
+pub mod logger;
 use serde_yaml::{self, Sequence};
+use std::{
+    convert::TryInto,
+    io::{BufRead, BufReader},
+    net::{SocketAddr, TcpListener},
+    thread::{self},
+};
 
-fn main() -> Result<(), Box<std::error::Error>> {
-    let f = std::fs::File::open("src/agents.yaml")?;
-    let d: Sequence = serde_yaml::from_reader(f)?;
-    for agent in d {
-        println!("{}", agent.get("name").unwrap().as_str().unwrap());
-        println!("{}", agent.get("successrate").unwrap().as_f64().unwrap());
-        println!("{}", agent.get("port").unwrap().as_i64().unwrap());
+fn create_listener(name: String, port: u16, successrate: f64) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener =
+        TcpListener::bind(addr).unwrap_or_else(|_| panic!("listener on port {} failed", port));
+
+    logger::log(
+        format!(
+            "Started {} on port {} with sucess rate {}",
+            name, port, successrate
+        ),
+        logger::LogLevel::INFO,
+    );
+
+    for stream in listener.incoming() {
+        let stream = stream.expect("failed to read stream");
+
+        let peer = stream
+            .peer_addr()
+            .expect("Couldn't read connection peer addr");
+        let mut reader = BufReader::new(stream);
+
+        thread::Builder::new()
+            .name(format!("{} - {}", name, peer.port()))
+            .spawn(move || loop {
+                let mut buffer = String::new();
+                reader.read_line(&mut buffer).expect("failed to read line");
+                if !buffer.is_empty() {
+                    print!("{}", buffer);
+                }
+            })
+            .expect("agent connection thread creation failed");
     }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    logger::init();
+    let agents_config =
+        std::fs::File::open("src/agents.yaml").expect("Couldn't open agents config file");
+    let agents: Sequence =
+        serde_yaml::from_reader(agents_config).expect("Couldn't parse agents config yaml");
+
+    let mut agents_threads = vec![];
+    for agent in agents {
+        let name = agent["name"]
+            .as_str()
+            .expect("Agent name must be a string")
+            .to_string();
+
+        let port = agent["port"]
+            .as_u64()
+            .expect("Agent port must be an unsigned integer")
+            .try_into()
+            .expect("Agent port must be a valid port number");
+
+        let successrate = agent["successrate"]
+            .as_f64()
+            .expect("Agent successrate must be a float");
+
+        agents_threads.push(
+            thread::Builder::new()
+                .name(name.clone())
+                .spawn(move || {
+                    create_listener(name, port, successrate);
+                })
+                .expect("agent thread creation failed"),
+        )
+    }
+
+    for thread in agents_threads {
+        thread.join().expect("agent thread join failed");
+    }
+
     Ok(())
 }
--- src/alglobo.rs
+++ src/alglobo.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use std::net::TcpStream;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub mod logger;
+use serde_yaml::{self, Sequence};
+use std::{
+    convert::TryInto,
+    net::SocketAddr,
+    thread::{self},
+};
+
+fn create_connection(port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let mut stream =
+        TcpStream::connect(addr).unwrap_or_else(|_| panic!("connection with port {} failed", port));
+
+    loop {
+        stream.write_all("100\n".as_bytes()).expect("write failed");
+        sleep(Duration::from_millis(1000));
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    logger::init();
+    let agents_config =
+        std::fs::File::open("src/agents.yaml").expect("Couldn't open agents config file");
+    let agents: Sequence =
+        serde_yaml::from_reader(agents_config).expect("Couldn't parse agents config yaml");
+
+    let mut agents_threads = vec![];
+    for agent in agents {
+        let port: u16 = agent["port"]
+            .as_u64()
+            .expect("Agent port must be an unsigned integer")
+            .try_into()
+            .expect("Agent port must be a valid port number");
+
+        agents_threads.push(
+            thread::Builder::new()
+                .name(port.to_string().clone())
+                .spawn(move || {
+                    create_connection(port);
+                })
+                .expect("agent connection thread creation failed"),
+        )
+    }
+
+    for thread in agents_threads {
+        thread.join().expect("agent thread join failed");
+    }
+
+    Ok(())
 }
