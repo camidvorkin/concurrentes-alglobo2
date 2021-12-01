1ecfc9e1e4cbc11f7f4fb701e033b22833fdb4bc
FdelMazo <fdelmazo@fi.uba.ar>
2021-12-01 18:40:19 -0300

    Remove logs from repo

--- src/logger.rs
+++ src/logger.rs
@@ -4,7 +4,6 @@ use std::io::Write;
 
 /// Logging file to be created (or entirely rewritten) on each run
 
-
 /// Simple logging levels for our logger
 #[derive(Debug, Clone, Copy)]
 pub enum LogLevel {
@@ -36,25 +35,27 @@ impl Logger {
     pub fn new(filename: String) -> Self {
         let mut filename_path = "logs/".to_string();
         filename_path.push_str(filename.as_str());
-        filename_path.push_str(".txt");
-     
-        let logger = Logger { filename: filename_path.clone() };
+        filename_path.push_str(".log");
+
+        let logger = Logger {
+            filename: filename_path.clone(),
+        };
         OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(filename_path.as_str())
-        .expect("Failed to create log file");
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(filename_path.as_str())
+            .expect("Failed to create log file");
         logger.log("START".to_string(), LogLevel::TRACE);
         logger
     }
-    
+
     /// Logs the message to the file and, if the level is INFO, prints to console
     pub fn log(&self, msg: String, loglevel: LogLevel) {
         let mut file = OpenOptions::new()
-        .append(true)
-        .open(self.filename.clone().as_str())
-        .expect("Unable to open log file");
+            .append(true)
+            .open(self.filename.clone().as_str())
+            .expect("Unable to open log file");
         if let LogLevel::INFO = loglevel {
             println!("{}", msg)
         };
@@ -67,5 +68,5 @@ impl Logger {
         let msg = format!("{} | {:<6} | {} \n", chrono::Local::now(), loglevelstr, msg);
         file.write_all(msg.as_bytes())
             .expect("Unable to write data");
-    }    
+    }
 }
