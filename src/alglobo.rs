7f90b9c8a0c7e122a75370c3de99e3c9fc186b38
Cami Dvorkin <cdvorkin@medallia.com>
2021-12-04 12:46:23 -0300

    Thread in alglobo for each transaction and each agent

--- src/alglobo.rs
+++ src/alglobo.rs
@@ -22,35 +22,6 @@ use utils::{agent_get_name, agent_get_port, csv_to_prices};
 
 const TIMEOUT: Duration = Duration::from_secs(10);
 
-fn create_connection(port: u16, agent_name: String, prices: Vec<HashMap<String, u32>>) {
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-
-    let mut stream =
-        TcpStream::connect(addr).unwrap_or_else(|_| panic!("connection with port {} failed", port));
-
-    for (i, price) in prices.iter().enumerate() {
-        let msg = DataMsg {
-            transaction_id: i as u32,
-            opcode: PREPARE,
-            data: price[&agent_name],
-        };
-
-        stream
-            .write_all(&data_msg_to_bytes(&msg))
-            .expect("write failed");
-        
-        // TODO: sacar sleep
-        sleep(Duration::from_millis(1000));
-
-        let mut response = [0u8; 1];
-        stream.read_exact(&mut response).expect("read failed");
-        println!("Received {:?}", response);
-    }
-}
-
-
-
-
 fn main() {
     // TODO: Make get_agents() return a vector of agents instead of a serde Sequence
     // We have to do everything in this function just to not import Sequence from sede
@@ -69,30 +40,37 @@ fn main() {
         agent_clients.push(client);
     }
 
-    
     for (transaction_id, price) in prices.iter().enumerate() {
-        // TODO: que se envien los tres mensajes en paralelo
-        responses.0.lock().unwrap().clear();
         for (j, agent) in agents.iter().enumerate() {
-            let msg = DataMsg {
-                transaction_id: transaction_id as u32,
-                opcode: PREPARE,
-                data: price[j]
-            };
+            let p = price[j];
+            // borrow client_agent[j]
+            // let client = &agent_clients[j];
+            let mut client = agent_clients.try_clone().unwrap();
+            thread::Builder::new()
+                .name(format!("{} - Transaction {}", agent_get_name(agent), transaction_id))
+                .spawn(move || {
+                    let msg = DataMsg {
+                        transaction_id: transaction_id as u32,
+                        opcode: PREPARE,
+                        data: p
+                    };
 
-            agent_clients[j].write_all(&data_msg_to_bytes(&msg)).expect("write failed");
-            
-            // TODO: sacar sleep
-            sleep(Duration::from_millis(1000));
+                    client.write_all(&data_msg_to_bytes(&msg)).expect("write failed");
+                    
+                    // TODO: sacar sleep
+                    // sleep(Duration::from_millis(1000));
 
-            let mut response = [0u8; 1];
-            agent_clients[j].read_exact(&mut response).expect("read failed");
-            println!("Received {:?}", response);
-            responses.0.lock().unwrap().push(Some(response));
+                    let mut response = [0u8; 1];
+                    client.read_exact(&mut response).expect("read failed");
+                    println!("Received {:?}", response);
+                    {
+                        responses.0.lock().unwrap().push(Some(response));
+                    }
+                });
         }
-
+        // responses.1.wait_timeout_while(self.responses.0.lock().unwrap(), TIMEOUT, |responses| responses.iter().any(Option::is_none));
         let response = if responses.0.lock().unwrap().iter().all(|opt| opt.is_some() && opt.unwrap()[0] == COMMIT) {COMMIT} else {ABORT};
-        for (j, agent) in agents.iter().enumerate() {
+        for (j, _agent) in agents.iter().enumerate() {
             let msg = DataMsg {
                 transaction_id: transaction_id as u32,
                 opcode: response,
